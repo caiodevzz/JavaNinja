853217cffc620f76eb69ff94e68030f3ca2da514
caiodevzz <caiomateus270@gmail.com>
2026-07-23 14:44:38 -0300

    exercios sobre matriz

---
+++ src/desafio/matrizes/exercio01.java
@@ -0,0 +1,31 @@
+package desafio.matrizes;
+
+import java.util.Scanner;
+
+public class exercio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int n = sc.nextInt();
+
+        System.out.print("Digite outro numero: ");
+        int m = sc.nextInt();
+
+        int[][] matriz = new int[n][m];
+        for (int i = 0; i <matriz.length ; i++) {
+            for (int j = 0; j < matriz[i].length ; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < m ; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
